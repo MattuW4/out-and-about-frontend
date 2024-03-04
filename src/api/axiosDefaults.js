b8fb11be4789fd61adc4044dc41f89e3ab0d8502
Matt Waters <enjoi_waters@hotmail.com>
2024-03-04 10:37:20 +0000

    11 Correct spelling mistake in axios file to connect front and back end

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://oaa-app-2b3d894f937e.herokuapp.com/"
-axios.defaults.headers.post["Content-type"] = "multipart/form-data"
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data"
 axios.defaults.withCredentials = true;
