beaca5f356c841f28cdb26715c18a6d493c98d18
Matt Waters <enjoi_waters@hotmail.com>
2024-03-06 17:00:00 +0000

    16 Creat add event icon, what's on, attending and sign out buttons when signed in. Create avatar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,51 @@ import logofav from "../assets/logofav.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>
+    const addEventIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/events/create"
+        >
+            <i className="fa-regular fa-calendar-plus"></i>Add event
+        </NavLink>
+    )
+
+    const loggedInIcons = <>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/whatson"
+        >
+            <i className="fa-solid fa-magnifying-glass"></i>What's on?
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/attending"
+        >
+            <i className="fa-solid fa-heart-circle-check"></i>Attending
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to="/"
+            onClick={() => { }}
+        >
+            <i className="fa-solid fa-person-through-window"></i>Sign out
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to={`/profiles/${currentUser?.profile_id}`}
+        >
+            <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+        </NavLink>
+
+    </>
     const loggedOutIcons = (<>
         <NavLink
             className={styles.NavLink}
@@ -35,6 +75,7 @@ const NavBar = () => {
                         <img src={logofav} alt="logo" height="95" />
                     </Navbar.Brand>
                 </NavLink>
+                {currentUser && addEventIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
