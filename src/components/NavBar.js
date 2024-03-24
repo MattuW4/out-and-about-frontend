2614b3e5eaf26e3ba47315710d1dce034b6c6efd
Matt Waters <enjoi_waters@hotmail.com>
2024-03-24 17:45:26 +0000

    64 Create site information modal in navbar when user is logged out and first visits the site

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown, Modal, Button } from "react-bootstrap";
 import logofav from "../assets/logofav.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
@@ -12,9 +12,12 @@ import axios from "axios";
 import { useClickOutsideToggle } from "../hooks/useClickOutsideToggle";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const NavBar = () => {
+const NavBar = (props) => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const handleClose = () => setShow(false);
+    const [show, setShow] = useState(true);
 
     const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
@@ -138,6 +141,54 @@ const NavBar = () => {
         >
             <i className="fa-solid fa-user-pen"></i>Sign up
         </NavLink>
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={show}
+            backdrop="static"
+            keyboard={false}
+            onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title className="align-items-center justify-content-between">
+                    <img src={logofav} alt="logo" height="95" />
+                    <br />
+                    Welcome to Out & About
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                We are a social platform for music and cultural events in your area and beyond.
+                You can see an overview of whats on without being a member but to be able to 
+                create events, subscribe to other users, leave comments, review past events 
+                and much more you need to sign up
+                <Link
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}
+                    to="/signup"
+                >
+                    <i className="fa-solid fa-user-pen"></i>
+                </Link>
+                <br />
+                If you are already part of the community, sign in
+                <Link
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}
+                    to="/signin"
+                >
+                    <i className="fa-solid fa-arrow-right-to-bracket"></i>
+                </Link>
+                <br />
+                This information only appears on the homepage when you first visit us so if you
+                want to see it again just refresh the page!
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+
+            </Modal.Footer>
+        </Modal>
     </>
     );
 
