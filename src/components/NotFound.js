f89cc54ba07f58b69079a2b7575d43dab4bb671c
Matt Waters <enjoi_waters@hotmail.com>
2024-03-29 10:28:59 +0000

    95 Add card body to 404 page to style

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,30 +4,35 @@ import styles from "../styles/NoResults.module.css";
 import NoResults from "../assets/no-results.webp";
 import Button from "react-bootstrap/Button";
 import btnStyles from "../styles/Button.module.css";
+import { Card } from "react-bootstrap";
 
 const NotFound = () => {
   return (
-    <>
-      <div className={styles.NotFound}>
-        <div className={styles.Error404}>404!</div>
-        <div className={styles.NotFoundText}>
-          Whoops! Page not found...
-        </div>
-        <div>
-          <Asset alt="page not found" src={NoResults} />
-        </div>
-        <div className={`text-center`}>
-          <Button 
-          className={`${btnStyles.Button} ${btnStyles.Purple}`}
-          aria-label="Return to homepage button"
-          >
-            <Link className={styles.Link} to={"/"}>
-              Click here to return to the Homepage
-            </Link>
-          </Button>
-        </div>
-      </div>
-    </>
+    <Card className={styles.Event}>
+      <Card.Body>
+        <>
+          <div className={styles.NotFound}>
+            <div className={styles.Error404}>404!</div>
+            <div className={styles.NotFoundText}>
+              Whoops! Page not found...
+            </div>
+            <div>
+              <Asset alt="page not found" src={NoResults} />
+            </div>
+            <div className={`text-center`}>
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Purple}`}
+                aria-label="Return to homepage button"
+              >
+                <Link className={styles.Link} to={"/"}>
+                  Click here to return to the Homepage
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </>
+      </Card.Body>
+    </Card>
   );
 };
 
